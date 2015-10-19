1b571091538852717144c52de21045a0e37b5781
ShridharAcharya <dude.acharya@gmail.com>
2015-10-18 22:54:53 -0400

    Implementation of makeCacheMatrix and getSolve functions.
    Implemented and successfully tested makeCacheMatrix and getSolve
    functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function makeCacheMatrix implements an object that stores a matrix
+## and its inverse. The function cacheSolve implements methods to compute
+## and cache the inverse of the matrix stored using makeCacheMatrix.
+## These functions together can be used to cache inverses of frquently used
+## matrices.
 
-## Write a short comment describing this function
+## makeCacheMatrix implements an object that stores a matrix and its inverse.
+## The function also implements set, get, setSolve and getSolve methods used to
+## set and access the stored matrix and to get and set the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        slv <- NULL
+        set <- function(y) {
+                x <<- y
+                slv <<- NULL
+        }
+        get <- function() x
+        setSolve <- function(solve) slv <<- solve
+        getSolve <- function() slv
+        list(set = set, get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
 }
 
-
-## Write a short comment describing this function
-
+##
+## The cacheSolve function when first called on an object made by makeCacheMatrix
+## computes the inverse of the matrix using "solve" function, stores/caches the 
+## inverse using setSolve() function and returns the inverse.
+## Subsequent calls to cacheSolve just return the cached value using the getSolve 
+## function.
+##
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		slv <- x$getSolve()
+        if(!is.null(slv)) {
+                message("getting cached data")
+                return(slv)
+        }
+        data <- x$get()
+        slv <- solve(data, ...)
+        x$setSolve(slv)
+        slv
 }
